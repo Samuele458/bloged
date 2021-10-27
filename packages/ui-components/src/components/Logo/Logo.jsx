7fefb69d92db5b94940d1f321fcf7a23e64509df
Samuele Girgenti <samuele.girgenti458@gmail.com>
2021-10-27 12:56:47 +0200

    init dotnet backend

--- packages/ui-components/src/components/Logo/Logo.jsx
+++
@@ -1,7 +0,0 @@
-import NormalLogo from "./images/normal-large.png";
-
-const Logo = (props) => {
-  return <img src={NormalLogo} alt="" className="app-logo" />;
-};
-
-export default Logo;
--- packages/ui-components/src/index.js
+++
@@ -1,6 +0,0 @@
-import Button from "./components/Button";
-import LoginForm from "./components/LoginForm";
-import SignUpForm from "./components/SignUpForm";
-import "./style/style.scss";
-
-export { Button, LoginForm, SignUpForm };
