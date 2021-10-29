98046ac78376e4b3c20ebd69d2f9801adf6ce342
Samuele Girgenti <samuele.girgenti458@gmail.com>
2021-10-29 16:57:09 +0200

    docker support for production

--- BlogedWebapp/Startup.cs
+++ BlogedWebapp/Startup.cs
@@ -51,8 +51,6 @@ namespace BlogedWebapp
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BlogedWebapp v1"));
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
