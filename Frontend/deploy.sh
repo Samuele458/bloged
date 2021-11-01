9d0bab4df769abe31ee236fd6947efb0958f760d
Samuele Girgenti <samuele.girgenti458@gmail.com>
2021-11-01 21:16:49 +0100

    update .gitignore file

--- Frontend/deploy.sh
+++ Frontend/deploy.sh
@@ -3,18 +3,8 @@
 #deploying build folders to wwwroot-folder volume
 rm -r -f /app/wwwroot/*
 
-echo app:
-ls -la /app
-
-echo wwwroot:
-ls /app/wwwroot
-
-
 
 #copying new files
 mkdir -p /app/wwwroot/dashboard
 cp -r /app/packages/blog/build/* /app/wwwroot/
 cp -r /app/packages/dashboard/build/* /app/wwwroot/dashboard/
-
-echo wwwroot:
-ls /app/wwwroot
---
+++ Frontend/packages/ui-components/src/index.js
@@ -0,0 +1,6 @@
+import Button from "./components/Button";
+import LoginForm from "./components/LoginForm";
+import SignUpForm from "./components/SignUpForm";
+import "./style/style.scss";
+
+export { Button, LoginForm, SignUpForm };
