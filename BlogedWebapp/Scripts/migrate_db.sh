68b13b63d8a743b08f01336d5b26df298a13e439
Samuele Girgenti <samuele.girgenti458@gmail.com>
2021-11-04 00:21:35 +0100

    fix

--- BlogedWebapp/Scripts/migrate_db.sh
+++ BlogedWebapp/Scripts/migrate_db.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 #checking for updating the database
+
+
 if [[ -z "${BLOGED_DB_CONN}" ]]; then
-  dotnet ef database update
-else
   echo "BLOGED_DB_CONN is not set. Continuing without updating database."
+else
+  dotnet ef database update
 fi
 
