3cbbdce61d8fa4720519758eec56b8fffdc7466c
Samuele Girgenti <samuele.girgenti458@gmail.com>
2021-12-15 02:52:38 +0100

    Tests

--- BlogedWebapp/Helpers/ProjectionHelper.cs
+++ BlogedWebapp/Helpers/ProjectionHelper.cs
@@ -40,14 +40,24 @@ namespace BlogedWebapp.Helpers
         Full
     }
 
-
+    /// <summary>
+    ///  This attribute, if present on a property, specifies if that
+    ///  property has to be handled as to be handled as a sub entity
+    ///  (and so will be processed for projection of its fields), or
+    ///  a normal field.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property, Inherited = true)]
     public class RelatedEntity : Attribute
     {
 
     }
 
-
+    /// <summary>
+    ///  Attribute for specifing the behaviour each
+    ///  property has to follow.
+    ///  If Projection attribute is not present, the behaviour for that
+    ///  property will be equal to [Projection(ProjectionBehaviour.Normal)]
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property, Inherited = true)]
     public class Projection : Attribute
     {
@@ -64,8 +74,37 @@ namespace BlogedWebapp.Helpers
         }
     }
 
+
+    /// <summary>
+    ///  Helper class for handling projections on Entity Framework
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public static class ProjectionHelper<T> where T : class
     {
+
+
+        public static Expression<Func<T, T>> BuildProjectionLambda(
+            IQueryable<T> dbset,
+            ProjectionBehaviour projectionBehaviour
+        )
+        {
+            var entityType = typeof(T);
+
+            // Parameter of lambda function
+            var entityParameter = Expression.Parameter(typeof(T), "o");
+
+            // Lambda function
+            var lambda = Expression.Lambda<Func<T, T>>(MakeEntityObject(entityType, entityParameter, projectionBehaviour), entityParameter);
+
+            return lambda;
+        }
+
+        /// <summary>
+        ///  Build a projection
+        /// </summary>
+        /// <param name="dbset">DBSet on which make projection</param>
+        /// <param name="projectionBehaviour">Projection behaviour</param>
+        /// <returns>A Queryable object with applied projection filter</returns>
         public static IQueryable<T> BuildProjection(
             IQueryable<T> dbset,
             ProjectionBehaviour projectionBehaviour
@@ -73,22 +112,39 @@ namespace BlogedWebapp.Helpers
         {
             var entityType = typeof(T);
 
+            // Parameter of lambda function
             var entityParameter = Expression.Parameter(typeof(T), "o");
 
+            // Lambda function
             var lambda = Expression.Lambda<Func<T, T>>(MakeEntityObject(entityType, entityParameter, projectionBehaviour), entityParameter);
 
+            // Debugging
             System.Diagnostics.Debug.WriteLine(lambda.ToString());
             System.Diagnostics.Debug.WriteLine(dbset.Select(lambda).ToQueryString());
 
+            // Applies projection and returns the result object (IQueryble)
             return dbset.Select(lambda);
 
         }
 
-        // public static Expression<Func<T, T>> MakeProjectionLambda()
-        //{
-
-        //}
-
+        /// <summary>
+        ///  Creates an entity object dynamically.
+        ///  Based on projectionBehaviour, MakeEntityObject can be applied recursively
+        ///  on sub fields, in order to be able to make projection also in sub entities.
+        ///  It is based on reflection.
+        /// </summary>
+        /// <param name="entityType">Entity type to be created</param>
+        /// <param name="objectToAssign">
+        ///  Expression object to be assigned to.
+        ///  eg. the parameter of a lambda function, or another object
+        ///  in case of a recursive call for filling sub entities
+        /// </param>
+        /// <param name="projectionBehaviour">
+        ///  Behaviour to follow in order to make the projection.
+        ///  It can allow to apply this method recursively to sub entities, in
+        ///  order to do projection also in them
+        /// </param>
+        /// <returns>The result expression</returns>
         public static Expression MakeEntityObject(
                 Type entityType,
                 Expression objectToAssign,
@@ -102,8 +158,10 @@ namespace BlogedWebapp.Helpers
             Dictionary<PropertyInfo, Projection> propertiesWithProjection =
                 new Dictionary<PropertyInfo, Projection>();
 
+            // Constructor
             var constructor = Expression.New(entityType);
 
+            // The list with all assignments for the current object
             List<MemberBinding> bindings = new List<MemberBinding>();
 
             foreach (var property in properties)
@@ -111,31 +169,45 @@ namespace BlogedWebapp.Helpers
                 var relatedEntityAttribute = property.GetCustomAttribute<RelatedEntity>(true);
                 var projectionAttribute = property.GetCustomAttribute<Projection>(true);
 
+                // Checking if property has Projection attribute.
+                // If not, it should be handled as ProjectionBehaviour.Normal
                 if (projectionAttribute == null)
                 {
                     projectionAttribute = new Projection(ProjectionBehaviour.Normal);
                 }
 
-
+                // Checking if current property is in projection, based on the ProjectionBehaviour to follow
                 if (IsInProjection(projectionBehaviour, projectionAttribute.Behaviour))
                 {
+                    // Property is in projection
+
                     var value = Expression.Property(objectToAssign, property);
                     MemberAssignment assignment;
 
+
+                    // Checking if property is a normal value or has the RelatedEntity attribute. 
                     if (relatedEntityAttribute == null)
                     {
+                        // Property is a normal attribute.
+                        // The assignment can be done just with the value.
                         assignment = Expression.Bind(property, value);
                     }
                     else
                     {
+                        // Property is a sub entity.
+                        // The assignment must be done by creating a new object
+                        // of the specified entity type. This is done by calling 
+                        // Recursively MakeEntityObject
                         assignment = Expression.Bind(property, MakeEntityObject(value.Type, value, projectionBehaviour));
                     }
 
+                    // Add assignment to bindings (the list with all assignments for the current object)
                     bindings.Add(assignment);
                 }
 
             }
 
+            // returns the object
             return Expression.MemberInit(constructor, bindings);
         }
 
---
+++ tests/UnitTests/Helpers/ProjectionHelperTests.cs
@@ -0,0 +1,102 @@
+﻿using BlogedWebapp.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace UnitTests.Helpers
+{
+    [TestClass]
+    public class ProjectionHelperTests
+    {
+
+        // Classes used for doing tests
+
+        public class TestEntity_WithProjectionAttributeOnProperty
+        {
+            [Projection(ProjectionBehaviour.Normal)]
+            public string Name { get; set; }
+        }
+
+        public class TestEntity_WithoutProjectionAttributeOnProperty
+        {
+            public string Name { get; set; }
+        }
+
+        public class TestEntity_WithRelatedEntityAttributeOnProperty
+        {
+            [RelatedEntity]
+            [Projection(ProjectionBehaviour.Normal)]
+            public TestEntity_WithProjectionAttributeOnProperty Entity { get; set; }
+        }
+
+        public class TestEntity_WithoutRelatedEntityAttributeOnProperty
+        {
+
+            [Projection(ProjectionBehaviour.Normal)]
+            public string Name { get; set; }
+        }
+
+
+        // --- Unit tests ---
+
+        [TestMethod]
+        public void ProjectionAttribute_PropertyWithProjectionAttribute_AttributeFound()
+        {
+            Type type = typeof(TestEntity_WithProjectionAttributeOnProperty);
+            var attribute = type.GetProperties()[0].GetCustomAttribute<Projection>(true);
+
+            Assert.IsNotNull(attribute);
+        }
+
+        [TestMethod]
+        public void ProjectionAttribute_PropertyWithoutProjectionAttribute_AttributeNotFound()
+        {
+            Type type = typeof(TestEntity_WithoutProjectionAttributeOnProperty);
+            var attribute = type.GetProperties()[0].GetCustomAttribute<Projection>(true);
+
+            Assert.IsNull(attribute);
+        }
+
+        [TestMethod]
+        public void RelatedEntityAttribute_PropertyWithRelatedEntityAttribute_AttributeFound()
+        {
+            Type type = typeof(TestEntity_WithRelatedEntityAttributeOnProperty);
+            var attribute = type.GetProperties()[0].GetCustomAttribute<RelatedEntity>(true);
+
+            Assert.IsNotNull(attribute);
+        }
+
+        [TestMethod]
+        public void RelatedEntityAttribute_PropertyWithoutRelatedEntityAttribute_AttributeNotFound()
+        {
+            Type type = typeof(TestEntity_WithoutRelatedEntityAttributeOnProperty);
+            var attribute = type.GetProperties()[0].GetCustomAttribute<RelatedEntity>(true);
+
+            Assert.IsNull(attribute);
+        }
+
+
+        private static string StringifyProjectionLambda<T>() where T : class
+        {
+            List<T> list = new List<T>();
+            IQueryable<T> queryable = list.AsQueryable();
+
+            var result = ProjectionHelper<T>
+                            .BuildProjectionLambda(queryable, ProjectionBehaviour.Normal)
+                            .ToString();
+            return result;
+        }
+
+        [TestMethod]
+        public void BuildProjectionLambda()
+        {
+            var result = StringifyProjectionLambda<TestEntity_WithProjectionAttributeOnProperty>();
+            var expectedResult = "o => new TestEntity_WithProjectionAttributeOnProperty() {Name = o.Name}";
+
+            Assert.IsTrue(result == expectedResult);
+        }
+
+    }
+}
